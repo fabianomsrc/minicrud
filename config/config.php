0500275099bea140da6b4d90c15359f5b46698e7
Fabiano M. Silva <fabiano.msrc@outlook.com>
2022-04-21 20:51:27 -0300

    Constantes para configuração do projeto

---
+++ config/config.php
@@ -0,0 +1,14 @@
+<?php
+
+// Project base URL without the last slash
+define('BASEURL', 'http://localhost/minicrud');
+
+define('APPNAME', ':: MiniCRUD ::');
+
+// Database connection credentials
+define('DB_HOST', '127.0.0.1');
+define('DB_PORT', 3308);
+define('DB_USER', 'minicrud');
+define('DB_PASS', 'DYrVAcGO!M#VV!7kw5Lf@aWG');
+define('DB_NAME', 'minicrud');
+define('CHARSET', 'utf8mb4');

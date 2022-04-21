3e6b142d12b38af10356786f30ad4777c5437452
Fabiano M. Silva <fabiano.msrc@outlook.com>
2022-04-21 20:53:34 -0300

    Páginas do CRUD

---
+++ pages/user/create.php
@@ -0,0 +1,59 @@
+<?php
+
+use MiniCRUD\Core\Person;
+use MiniCRUD\Database\MySQLManager;
+
+require __DIR__ . '/../../vendor/autoload.php';
+include __DIR__ . '/../partials/header.php';
+
+if (filter_input_array(INPUT_POST)) {
+    $person = new Person(new MySQLManager());
+
+    $post = filter_input_array(INPUT_POST);
+
+    $data = [
+        'name'  => trim($post['name']),
+        'email' => trim($post['email']),
+        'phone' => trim($post['phone'])
+    ];
+
+    $data = (object)$data;
+
+    if (! empty($data->name) && ! empty($data->email)) {
+        $person->name  = $data->name;
+        $person->email = $data->email;
+        $person->phone = $data->phone;
+
+        if (! $person->findPersonByEmail($data->email)) {
+            $person->insertNewPerson();
+            header('Location: ./read.php?message=success');
+        } else {
+            echo 'Email already registered!';
+        }
+    }
+}
+
+?>
+
+<div>
+    <div>
+        <a href="./../../index.php"><h1>:: Create ::</h1></a>
+        <a href="./../../index.php">Preview</a>
+    </div>
+    <div>
+        <div>
+            <form action="./../user/create.php" method="post">
+                <label for="name">Name</label>
+                <input type="text" name="name" id="name" placeholder="Full Name">
+                <label for="name">Email</label>
+                <input type="email" name="email" id="email" placeholder="email@example.com">
+                <label for="name">Phone</label>
+                <input type="text" name="phone" id="phone" placeholder="+5500000000000">
+
+                <button type="submit">Save</button>
+            </form>
+        </div>
+    </div>
+</div>
+
+<?php include __DIR__ . '/../partials/header.php' ?>
---
+++ pages/user/read.php
@@ -0,0 +1,37 @@
+<?php
+
+use MiniCRUD\Core\Person;
+use MiniCRUD\Database\MySQLManager;
+
+require __DIR__ . '/../../vendor/autoload.php';
+include __DIR__ . '/../partials/header.php';
+
+?>
+
+<div>
+    <div>
+        <h1>Read all Person</h1>
+        <a href="./create.php">New Person</a>
+    </div>
+    <div>
+        
+    </div>
+    <table>
+        <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Phone</th>
+        </tr>
+        <?php foreach ((new Person(new MySQLManager()))->readAllData() as $result) : ?>
+        <tr>
+            <td><?= htmlspecialchars($result['person_name'], ENT_QUOTES) ?></td>
+            <td><?= htmlspecialchars($result['person_email'], ENT_QUOTES) ?></td>
+            <td><?= htmlspecialchars($result['person_phone'], ENT_QUOTES) ?></td>
+            <td><a href="./update.php?personID=<?=$result['person_id']?>"><i class="fas fa-edit"></i></a></td>
+            <td><a href="./delete.php?personID=<?=$result['person_id']?>"><i class="fas fa-trash"></i></a></td>
+        </tr>
+        <?php endforeach ?>
+    </table>
+</div>
+
+<?php include __DIR__ . '/../partials/footer.php' ?>
